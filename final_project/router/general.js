b3e623c7c3457b6cca98ac31ac9569b112fe8f31
Hedy Yang <hedyyang1004@gmail.com>
2023-05-25 16:28:17 -0400

    Implement GET book review method

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -46,8 +46,8 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let reviews = books[req.params.isbn]["reviews"];
+  res.send(reviews);
 });
 
 module.exports.general = public_users;
