a664a19d9624fb0a72cc41db83bacc128026194f
Hedy Yang <hedyyang1004@gmail.com>
2023-05-25 15:57:00 -0400

    Implement GET books by author method

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,8 +22,14 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let books_byAuthor = {};
+  let keys = Object.keys(books);
+  keys.forEach((key) => {
+    if (books[key]["author"] === req.params.author) {
+      books_byAuthor[key] = books[key];
+    }
+  })
+  res.send(JSON.stringify({ books_byAuthor }, null, 4))
 });
 
 // Get all books based on title
