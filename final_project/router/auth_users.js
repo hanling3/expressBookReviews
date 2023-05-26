8e1b62dc1be17ac5b7863cec6764867880259040
Hedy Yang <hedyyang1004@gmail.com>
2023-05-25 22:17:44 -0400

    Implement login and edit book review methods

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,69 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
+  let existingName = users.filter((user) => {
+    return user.username === username;
+  });
+  if (existingName.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  //write code to check if username and password match the one we have in records.
+  let validatedUsers = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if (validatedUsers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access');
+
+    req.session.authorization = {
+      accessToken, username
+    }
+
+    return res.status(200).send("User sccessfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book = books[isbn];
+  if (book) {
+    let new_review = req.body.reviews;
+    let reviewer = req.body.reviewer;
+    let reviews = books[isbn]["reviews"];
+    if (new_review) {
+      reviews[reviewer] = new_review;
+      res.send(`Book ${books[isbn]["title"]} has new reviews: `
+        + JSON.stringify({ reviews }, null, 4));
+    }
+  }
+  res.send("Unable to find book");
 });
 
 module.exports.authenticated = regd_users;
